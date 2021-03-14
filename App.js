c1dbac92c6ec1fe27918432abe8e1437192c6d3b
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-03-14 16:19:11 -0300

    ajustes

--- App.js
+++ App.js
@@ -1,117 +1,21 @@
-import React,{useState} from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import Slider from '@react-native-community/slider';
-import Clipboard from 'expo-clipboard';
-
-let charset = 'dsfsdf8323823cmifjf2199129321'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [password, setPassword] = useState('');
-  const [size, setSize] = useState(10)
-  function generatePass(){
-    
-
-    let pass = '';
-    for(let i = 0, n = charset.length; i < size; i++) {
-      pass += charset.charAt(Math.floor(Math.random() * n));
-    }
-
-    setPassword(pass);
-
-  }
-
-  function copyPass() {
-    Clipboard.setString(password);
-    alert("Senha copiada com sucesso")
-  }
-
-
-
   return (
     <View style={styles.container}>
-      <Image
-        source={require('./src/assets/logo.png')}
-        style={styles.logo}
-      />
-
-      <Text style={styles.title}>{size} Caracteres</Text>
-
-      <Text style={styles.title}></Text>
-
-      <View style={styles.area}>
-        <Slider
-          styles={{ height: 50 }}
-          minimumValue={5}
-          maximumValue={15}
-          minimumTrackTintColor="#ff0000"
-          maximumTrackTintColor="#000"
-          value={size}
-          onValueChange={(valor) => setSize(valor.toFixed(0))}
-        />
-      </View>
-
-      <TouchableOpacity style={styles.button} onPress={generatePass}>
-        <Text style={styles.buttonText}>Gerar senha</Text>
-      </TouchableOpacity>
-
-      {password !== '' && (
-        <View style={styles.area}>
-          <Text style={styles.password} onLongPress={copyPass}>{password}</Text>
-        </View>
-      )}
-      
-
-
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#f3f3ff'
-  },
-
-  logo: {
-    marginBottom: 60
-  },
-
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-
-  area: {
-    marginTop: 15,
-    marginBottom: 15,
     backgroundColor: '#fff',
-    width: '90%',
-    borderRadius: 7
-  },
-
-  button: {
-    backgroundColor: '#FFA200',
-    width: '80%',
-    height: 50,
-    justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 7,
-    marginBottom: 25
+    justifyContent: 'center',
   },
-
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-    fontWeight: 'bold'
-  },
-
-  password: {
-    padding: 10,
-    textAlign: 'center',
-    fontSize: 20
-  }
-
-
-})
+});
