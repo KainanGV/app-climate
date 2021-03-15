c9dd306005d1a1eb2ffd16128c090d2e6530c580
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-03-15 00:15:29 -0300

    ajustes do útimo comite

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes';
 
 export default function App() {
+  // status bar componente da barra superior do dispositivo
+
   return (
+
+    // Criamos um container de navegação por meio do componente Navigation
     <NavigationContainer>
       <StatusBar 
         hidden={true}
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,14 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from './pages/Home';
 import Search from './pages/Search';
 
+// Linha que tras todas as propriedades e métodos do react-navigation/drawer
 const Drawer = createDrawerNavigator();
 
 function Routes() {
     return (
+        // Menu de navegação escolhemos o Drawer
+        // Cada item deste é um Drawer Screen
+
         <Drawer.Navigator>
             <Drawer.Screen
                 name="Home"
