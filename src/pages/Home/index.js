73b2699e543e39b29b469fc8f42ca9aa5a1ce1cb
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-03-15 00:23:43 -0300

    style de minha cidade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
 
 export default function Home() {
     return (
-        <View>
+        // Área segura para dispositivos novos na parte superior
+        <SafeAreaView style={styles.container}>
             <Text>Home page</Text>
-        </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8f0ff',
+        paddingTop: '5%'
+    }
+})
